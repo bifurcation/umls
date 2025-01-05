f841f93d7d652e5828100b5a2d2e654d9d41bc4b
Richard Barnes <rlb@ipv.sx>
2025-01-04 17:45:33 -1000

    Make a macro for signed structs

--- src/protocol.rs
+++ src/protocol.rs
@@ -7,7 +7,6 @@ use crate::{mls_enum, mls_newtype_opaque, mls_newtype_primitive, mls_struct};
 use core::marker::PhantomData;
 use core::ops::{Deref, DerefMut};
 use heapless::Vec;
-use rand_core::CryptoRngCore;
 
 mod consts {
     use super::{CredentialType, ProtocolVersion};
@@ -24,6 +23,113 @@ mod consts {
     pub const MAX_EXTENSION_SIZE: usize = 128;
     pub const MAX_EXTENSIONS: usize = 0;
     pub const MAX_GROUP_ID_SIZE: usize = 16;
+}
+
+// A macro to generate signed data structures
+macro_rules! mls_signed {
+    ($signed_owned_type:ident + $signed_view_type:ident + $label:literal => 
+     $val_owned_type:ident + $val_view_type:ident) => {
+        #[derive(Clone, Default, Debug, PartialEq)]
+        pub struct $signed_owned_type {
+            to_be_signed: $val_owned_type,
+            to_be_signed_raw: Vec<u8, { <$val_owned_type>::MAX_SIZE }>,
+            signature: Signature,
+        }
+
+        #[derive(Clone, Debug, PartialEq)]
+        pub struct $signed_view_type<'a> {
+            to_be_signed: $val_view_type<'a>,
+            to_be_signed_raw: &'a [u8],
+            signature: SignatureView<'a>,
+        }
+        impl $signed_owned_type {
+            const SIGNATURE_LABEL: &[u8] = $label;
+
+            fn new(
+                to_be_signed: $val_owned_type,
+                signature_priv: SignaturePrivateKeyView,
+            ) -> Result<$signed_owned_type> {
+                // Serialize the part to be signed
+                let mut to_be_signed_raw = Vec::new();
+                to_be_signed.serialize(&mut to_be_signed_raw)?;
+
+                // Populate the signature
+                let signature = crypto::sign_with_label(
+                    &to_be_signed_raw,
+                    Self::SIGNATURE_LABEL,
+                    signature_priv,
+                )?;
+
+                let group_info = $signed_owned_type {
+                    to_be_signed,
+                    to_be_signed_raw,
+                    signature,
+                };
+                Ok(group_info)
+            }
+        }
+
+        impl<'a> $signed_view_type<'a> {
+            fn verify(&self, signature_key: SignaturePublicKeyView) -> Result<bool> {
+                crypto::verify_with_label(
+                    self.to_be_signed_raw.as_ref(),
+                    $signed_owned_type::SIGNATURE_LABEL,
+                    signature_key,
+                    self.signature.clone(),
+                )
+            }
+        }
+
+        impl Serialize for $signed_owned_type {
+            const MAX_SIZE: usize = sum(&[$val_owned_type::MAX_SIZE, Signature::MAX_SIZE]);
+
+            fn serialize(&self, writer: &mut impl Write) -> Result<()> {
+                self.to_be_signed.serialize(writer)?;
+                self.signature.serialize(writer)?;
+                Ok(())
+            }
+        }
+
+        impl<'a> Deserialize<'a> for $signed_view_type<'a> {
+            fn deserialize(reader: &mut impl ReadRef<'a>) -> Result<Self> {
+                let mut sub_reader = reader.fork();
+
+                let to_be_signed = $val_view_type::deserialize(&mut sub_reader)?;
+                let to_be_signed_raw = reader.read_ref(sub_reader.position())?;
+                let signature = SignatureView::deserialize(reader)?;
+
+                Ok(Self {
+                    to_be_signed,
+                    to_be_signed_raw,
+                    signature,
+                })
+            }
+        }
+
+        impl AsView for $signed_owned_type {
+            type View<'a> = $signed_view_type<'a>;
+
+            fn as_view<'a>(&'a self) -> Self::View<'a> {
+                Self::View {
+                    to_be_signed: self.to_be_signed.as_view(),
+                    to_be_signed_raw: &self.to_be_signed_raw,
+                    signature: self.signature.as_view(),
+                }
+            }
+        }
+
+        impl<'a> ToOwned for $signed_view_type<'a> {
+            type Owned = $signed_owned_type;
+
+            fn to_owned(&self) -> Self::Owned {
+                Self::Owned {
+                    to_be_signed: self.to_be_signed.to_owned(),
+                    to_be_signed_raw: self.to_be_signed_raw.try_into().unwrap(),
+                    signature: self.signature.to_owned(),
+                }
+            }
+        }
+    };
 }
 
 // Credentials
@@ -133,139 +239,7 @@ mls_struct! {
     signature_priv: SignaturePrivateKey + SignaturePrivateKeyView,
 }
 
-#[derive(Clone, Default, Debug, PartialEq)]
-struct LeafNode {
-    to_be_signed: LeafNodeTbs,
-    to_be_signed_raw: Vec<u8, { Self::MAX_SIZE }>,
-    signature: Signature,
-}
-
-#[derive(Clone, Debug, PartialEq)]
-struct LeafNodeView<'a> {
-    to_be_signed: LeafNodeTbsView<'a>,
-    to_be_signed_raw: &'a [u8],
-    signature: SignatureView<'a>,
-}
-
-impl LeafNode {
-    const SIGNATURE_LABEL: &[u8] = b"LeafNodeTBS";
-
-    fn new(
-        rng: &mut impl CryptoRngCore,
-        leaf_node_source: LeafNodeSource,
-        signature_priv: SignaturePrivateKey,
-        signature_key: SignaturePublicKey,
-        credential: Credential,
-    ) -> Result<(LeafNodePriv, LeafNode)> {
-        // Construct the TBS object
-        let mut capabilities = Capabilities::default();
-        capabilities
-            .versions
-            .push(consts::SUPPORTED_VERSION)
-            .map_err(|_| Error("Too many items"))?;
-        capabilities
-            .cipher_suites
-            .push(crypto::CIPHER_SUITE)
-            .map_err(|_| Error("Too many items"))?;
-        capabilities
-            .credentials
-            .push(consts::SUPPORTED_CREDENTIAL_TYPE)
-            .map_err(|_| Error("Too many items"))?;
-
-        let (encryption_priv, encryption_key) = crypto::generate_hpke(rng)?;
-
-        let to_be_signed = LeafNodeTbs {
-            encryption_key,
-            signature_key,
-            credential,
-            leaf_node_source,
-            ..Default::default()
-        };
-
-        // Serialize the part to be signed
-        let mut to_be_signed_raw = Vec::new();
-        to_be_signed.serialize(&mut to_be_signed_raw)?;
-
-        // Populate the signature
-        let signature = crypto::sign_with_label(
-            &to_be_signed_raw,
-            Self::SIGNATURE_LABEL,
-            signature_priv.as_view(),
-        )?;
-
-        let leaf_node_priv = LeafNodePriv {
-            encryption_priv,
-            signature_priv,
-        };
-        let leaf_node = LeafNode {
-            to_be_signed,
-            to_be_signed_raw,
-            signature,
-        };
-        Ok((leaf_node_priv, leaf_node))
-    }
-}
-
-impl<'a> LeafNodeView<'a> {
-    fn verify(&self) -> Result<bool> {
-        crypto::verify_with_label(
-            self.to_be_signed_raw.as_ref(),
-            LeafNode::SIGNATURE_LABEL,
-            self.to_be_signed.signature_key.clone(),
-            self.signature.clone(),
-        )
-    }
-}
-
-impl Serialize for LeafNode {
-    const MAX_SIZE: usize = sum(&[LeafNodeTbs::MAX_SIZE, Signature::MAX_SIZE]);
-
-    fn serialize(&self, writer: &mut impl Write) -> Result<()> {
-        self.to_be_signed.serialize(writer)?;
-        self.signature.serialize(writer)?;
-        Ok(())
-    }
-}
-
-impl<'a> Deserialize<'a> for LeafNodeView<'a> {
-    fn deserialize(reader: &mut impl ReadRef<'a>) -> Result<Self> {
-        let mut sub_reader = reader.fork();
-
-        let to_be_signed = LeafNodeTbsView::deserialize(&mut sub_reader)?;
-        let to_be_signed_raw = reader.read_ref(sub_reader.position())?;
-        let signature = SignatureView::deserialize(reader)?;
-
-        Ok(Self {
-            to_be_signed,
-            to_be_signed_raw,
-            signature,
-        })
-    }
-}
-
-impl AsView for LeafNode {
-    type View<'a> = LeafNodeView<'a>;
-
-    fn as_view<'a>(&'a self) -> Self::View<'a> {
-        Self::View {
-            to_be_signed: self.to_be_signed.as_view(),
-            to_be_signed_raw: &self.to_be_signed_raw,
-            signature: self.signature.as_view(),
-        }
-    }
-}
-
-impl<'a> ToOwned for LeafNodeView<'a> {
-    type Owned = LeafNode;
-
-    fn to_owned(&self) -> Self::Owned {
-        Self::Owned {
-            to_be_signed: self.to_be_signed.to_owned(),
-            to_be_signed_raw: self.to_be_signed_raw.try_into().unwrap(),
-            signature: self.signature.to_owned(),
-        }
-    }
-}
+mls_signed! { LeafNode + LeafNodeView + b"LeafNodeTBS" => LeafNodeTbs + LeafNodeTbsView }
 
 // KeyPackage
 mls_struct! {
@@ -283,136 +257,7 @@ mls_struct! {
     leaf_node_priv: LeafNodePriv + LeafNodePrivView,
 }
 
-#[derive(Clone, Default, Debug, PartialEq)]
-pub struct KeyPackage {
-    to_be_signed: KeyPackageTbs,
-    to_be_signed_raw: Vec<u8, { Self::MAX_SIZE }>,
-    signature: Signature,
-}
-
-#[derive(Clone, Debug, PartialEq)]
-pub struct KeyPackageView<'a> {
-    to_be_signed: KeyPackageTbsView<'a>,
-    to_be_signed_raw: &'a [u8],
-    signature: SignatureView<'a>,
-}
-
-impl KeyPackage {
-    const SIGNATURE_LABEL: &[u8] = b"KeyPackageTBS";
-
-    fn new(
-        rng: &mut impl CryptoRngCore,
-        signature_priv: SignaturePrivateKey,
-        signature_key: SignaturePublicKey,
-        credential: Credential,
-    ) -> Result<(KeyPackagePriv, KeyPackage)> {
-        // Create the KeyPackageTbs
-        let (init_priv, init_key) = crypto::generate_hpke(rng)?;
-
-        let (leaf_node_priv, leaf_node) = LeafNode::new(
-            rng,
-            LeafNodeSource::KeyPackage(Lifetime::default()),
-            signature_priv.clone(),
-            signature_key,
-            credential,
-        )?;
-
-        let to_be_signed = KeyPackageTbs {
-            protocol_version: consts::SUPPORTED_VERSION,
-            cipher_suite: crypto::CIPHER_SUITE,
-            init_key,
-            leaf_node,
-            ..Default::default()
-        };
-
-        // Serialize the part to be signed
-        let mut to_be_signed_raw = Vec::new();
-        to_be_signed.serialize(&mut to_be_signed_raw)?;
-
-        // Populate the signature
-        let signature = crypto::sign_with_label(
-            &to_be_signed_raw,
-            Self::SIGNATURE_LABEL,
-            signature_priv.as_view(),
-        )?;
-
-        let key_package_priv = KeyPackagePriv {
-            init_priv,
-            leaf_node_priv,
-        };
-        let key_package = KeyPackage {
-            to_be_signed,
-            to_be_signed_raw,
-            signature,
-        };
-        Ok((key_package_priv, key_package))
-    }
-}
-
-impl<'a> KeyPackageView<'a> {
-    fn verify(&self) -> Result<bool> {
-        crypto::verify_with_label(
-            self.to_be_signed_raw.as_ref(),
-            KeyPackage::SIGNATURE_LABEL,
-            self.to_be_signed
-                .leaf_node
-                .to_be_signed
-                .signature_key
-                .clone(),
-            self.signature.clone(),
-        )
-    }
-}
-
-impl Serialize for KeyPackage {
-    const MAX_SIZE: usize = sum(&[KeyPackageTbs::MAX_SIZE, Signature::MAX_SIZE]);
-
-    fn serialize(&self, writer: &mut impl Write) -> Result<()> {
-        self.to_be_signed.serialize(writer)?;
-        self.signature.serialize(writer)?;
-        Ok(())
-    }
-}
-
-impl<'a> Deserialize<'a> for KeyPackageView<'a> {
-    fn deserialize(reader: &mut impl ReadRef<'a>) -> Result<Self> {
-        let mut sub_reader = reader.fork();
-
-        let to_be_signed = KeyPackageTbsView::deserialize(&mut sub_reader)?;
-        let to_be_signed_raw = reader.read_ref(sub_reader.position())?;
-        let signature = SignatureView::deserialize(reader)?;
-
-        Ok(Self {
-            to_be_signed,
-            to_be_signed_raw,
-            signature,
-        })
-    }
-}
-
-impl AsView for KeyPackage {
-    type View<'a> = KeyPackageView<'a>;
-
-    fn as_view<'a>(&'a self) -> Self::View<'a> {
-        Self::View {
-            to_be_signed: self.to_be_signed.as_view(),
-            to_be_signed_raw: &self.to_be_signed_raw,
-            signature: self.signature.as_view(),
-        }
-    }
-}
-
-impl<'a> ToOwned for KeyPackageView<'a> {
-    type Owned = KeyPackage;
-
-    fn to_owned(&self) -> Self::Owned {
-        Self::Owned {
-            to_be_signed: self.to_be_signed.to_owned(),
-            to_be_signed_raw: self.to_be_signed_raw.try_into().unwrap(),
-            signature: self.signature.to_owned(),
-        }
-    }
-}
+mls_signed! { KeyPackage + KeyPackageView + b"KeyPackageTBS" => KeyPackageTbs + KeyPackageTbsView }
 
 // GroupInfo
 mls_newtype_opaque! {
@@ -443,104 +288,7 @@ mls_struct! {
     signer: LeafIndex + LeafIndexView,
 }
 
-#[derive(Clone, Default, Debug, PartialEq)]
-struct GroupInfo {
-    to_be_signed: GroupInfoTbs,
-    to_be_signed_raw: Vec<u8, { Self::MAX_SIZE }>,
-    signature: Signature,
-}
-
-#[derive(Clone, Debug, PartialEq)]
-struct GroupInfoView<'a> {
-    to_be_signed: GroupInfoTbsView<'a>,
-    to_be_signed_raw: &'a [u8],
-    signature: SignatureView<'a>,
-}
-
-impl GroupInfo {
-    const SIGNATURE_LABEL: &[u8] = b"GroupInfoTBS";
-
-    fn new(
-        to_be_signed: GroupInfoTbs,
-        signature_priv: SignaturePrivateKeyView,
-    ) -> Result<GroupInfo> {
-        // Serialize the part to be signed
-        let mut to_be_signed_raw = Vec::new();
-        to_be_signed.serialize(&mut to_be_signed_raw)?;
-
-        // Populate the signature
-        let signature =
-            crypto::sign_with_label(&to_be_signed_raw, Self::SIGNATURE_LABEL, signature_priv)?;
-
-        let group_info = GroupInfo {
-            to_be_signed,
-            to_be_signed_raw,
-            signature,
-        };
-        Ok(group_info)
-    }
-}
-
-impl<'a> GroupInfoView<'a> {
-    fn verify(&self, signature_key: SignaturePublicKeyView) -> Result<bool> {
-        crypto::verify_with_label(
-            self.to_be_signed_raw.as_ref(),
-            GroupInfo::SIGNATURE_LABEL,
-            signature_key,
-            self.signature.clone(),
-        )
-    }
-}
-
-impl Serialize for GroupInfo {
-    const MAX_SIZE: usize = sum(&[GroupInfoTbs::MAX_SIZE, Signature::MAX_SIZE]);
-
-    fn serialize(&self, writer: &mut impl Write) -> Result<()> {
-        self.to_be_signed.serialize(writer)?;
-        self.signature.serialize(writer)?;
-        Ok(())
-    }
-}
-
-impl<'a> Deserialize<'a> for GroupInfoView<'a> {
-    fn deserialize(reader: &mut impl ReadRef<'a>) -> Result<Self> {
-        let mut sub_reader = reader.fork();
-
-        let to_be_signed = GroupInfoTbsView::deserialize(&mut sub_reader)?;
-        let to_be_signed_raw = reader.read_ref(sub_reader.position())?;
-        let signature = SignatureView::deserialize(reader)?;
-
-        Ok(Self {
-            to_be_signed,
-            to_be_signed_raw,
-            signature,
-        })
-    }
-}
-
-impl AsView for GroupInfo {
-    type View<'a> = GroupInfoView<'a>;
-
-    fn as_view<'a>(&'a self) -> Self::View<'a> {
-        Self::View {
-            to_be_signed: self.to_be_signed.as_view(),
-            to_be_signed_raw: &self.to_be_signed_raw,
-            signature: self.signature.as_view(),
-        }
-    }
-}
-
-impl<'a> ToOwned for GroupInfoView<'a> {
-    type Owned = GroupInfo;
-
-    fn to_owned(&self) -> Self::Owned {
-        Self::Owned {
-            to_be_signed: self.to_be_signed.to_owned(),
-            to_be_signed_raw: self.to_be_signed_raw.try_into().unwrap(),
-            signature: self.signature.to_owned(),
-        }
-    }
-}
+mls_signed! { GroupInfo + GroupInfoView + b"GroupInfoTBS" => GroupInfoTbs + GroupInfoTbsView }
 
 // TODO(RLB): Stub structs below this line
 
@@ -564,69 +312,28 @@ mod test {
     use crate::io::SliceReader;
     use crate::make_storage;
 
-    #[test]
-    fn leaf_node_sign_verify() {
-        let rng = &mut rand::thread_rng();
-
-        let (signature_priv, signature_key) = crypto::generate_sig(rng).unwrap();
-        let credential = Credential::default();
-
-        let (_leaf_node_priv, leaf_node) = LeafNode::new(
-            rng,
-            LeafNodeSource::default(),
-            signature_priv,
-            signature_key,
-            credential,
-        )
-        .unwrap();
-
-        let mut storage = make_storage!(LeafNode);
-        leaf_node.serialize(&mut storage).unwrap();
-
-        let mut reader = SliceReader::new(&storage);
-        let leaf_node_view = LeafNodeView::deserialize(&mut reader).unwrap();
-
-        let ver = leaf_node_view.verify().unwrap();
-        assert!(ver);
+    macro_rules! test_sign_verify {
+        ($signed_owned_type:ident, $signed_view_type:ident, $tbs_owned_type:ident) => {
+            let rng = &mut rand::thread_rng();
+            let (signature_priv, signature_key) = crypto::generate_sig(rng).unwrap();
+            let tbs = $tbs_owned_type::default();
+            let signed = $signed_owned_type::new(tbs, signature_priv.as_view()).unwrap();
+         
+            let mut storage = make_storage!($signed_owned_type);
+            signed.serialize(&mut storage).unwrap();
+         
+            let mut reader = SliceReader::new(&storage);
+            let view = $signed_view_type::deserialize(&mut reader).unwrap();
+         
+            let ver = view.verify(signature_key.as_view()).unwrap();
+            assert!(ver);
+        }
     }
 
     #[test]
-    fn key_package_sign_verify() {
-        let rng = &mut rand::thread_rng();
-
-        let (signature_priv, signature_key) = crypto::generate_sig(rng).unwrap();
-        let credential = Credential::default();
-
-        let (_key_package_priv, key_package) =
-            KeyPackage::new(rng, signature_priv, signature_key, credential).unwrap();
-
-        let mut storage = make_storage!(KeyPackage);
-        key_package.serialize(&mut storage).unwrap();
-
-        let mut reader = SliceReader::new(&storage);
-        let key_package_view = KeyPackageView::deserialize(&mut reader).unwrap();
-
-        let ver = key_package_view.verify().unwrap();
-        assert!(ver);
-    }
-
-    #[test]
-    fn group_info_sign_verify() {
-        let rng = &mut rand::thread_rng();
-
-        let (signature_priv, signature_key) = crypto::generate_sig(rng).unwrap();
-
-        let to_be_signed = GroupInfoTbs::default();
-
-        let group_info = GroupInfo::new(to_be_signed, signature_priv.as_view()).unwrap();
-
-        let mut storage = make_storage!(GroupInfo);
-        group_info.serialize(&mut storage).unwrap();
-
-        let mut reader = SliceReader::new(&storage);
-        let group_info_view = GroupInfoView::deserialize(&mut reader).unwrap();
-
-        let ver = group_info_view.verify(signature_key.as_view()).unwrap();
-        assert!(ver);
+    fn signed_objects() {
+        test_sign_verify!(LeafNode, LeafNodeView, LeafNodeTbs);
+        test_sign_verify!(KeyPackage, KeyPackageView, KeyPackageTbs);
+        test_sign_verify!(GroupInfo, GroupInfoView, GroupInfoTbs);
     }
 }
