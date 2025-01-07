c782492ac76d73f290f45e93387605320d701bb8
Richard Barnes <rlb@ipv.sx>
2025-01-07 11:21:11 -1000

    Refactor key schedule

--- src/key_schedule.rs
+++ src/key_schedule.rs
@@ -4,11 +4,20 @@ use crate::io::*;
 use crate::protocol::*;
 use crate::syntax::*;
 use crate::tree_math::*;
-use crate::{make_storage, serialize};
+use crate::{make_storage, mls_newtype_opaque, serialize};
 
+use core::ops::{Deref, DerefMut};
 use heapless::Vec;
 
 /*
+                    epoch_secret[n-1]
+                          |
+                          |
+                          V
+                    DeriveSecret(., "init")
+                          |
+                          |
+                          V
                     init_secret_[n-1]
                           |
                           |
@@ -40,100 +49,84 @@ psk_secret (or 0) --> KDF.Extract
                           |
                           |
                           V
-                     epoch_secret
+                     epoch_secret[n]
                           |
                           |
                           +--> DeriveSecret(., <label>)
-                          |    = <secret>
-                          |
-                          V
-                    DeriveSecret(., "init")
-                          |
-                          |
-                          V
-                    init_secret_[n]
+                               = <secret>
+
+Salient paths through the graph:
+
+epoch + commit + gc --> epoch + joiner + welcome
+
+joiner --> member + welcome
+
+member + gc --> epoch
+
 */
 
-#[derive(Clone, Default, PartialEq, Debug)]
-pub struct InitSecret(HashOutput);
+mls_newtype_opaque! { EpochSecret + EpochSecretView, crypto::consts::HASH_OUTPUT_SIZE }
+mls_newtype_opaque! { JoinerSecret + JoinerSecretView, crypto::consts::HASH_OUTPUT_SIZE }
+mls_newtype_opaque! { MemberSecret + MemberSecretView, crypto::consts::HASH_OUTPUT_SIZE }
+mls_newtype_opaque! { WelcomeSecret + WelcomeSecretView, crypto::consts::HASH_OUTPUT_SIZE }
 
-impl Serialize for InitSecret {
-    const MAX_SIZE: usize = HashOutput::MAX_SIZE;
+macro_rules! to_from_hash_output {
+    ($owned_type:ident + $view_type:ident) => {
+        impl From<$owned_type> for HashOutput {
+            fn from(val: $owned_type) -> Self {
+                Self::from(val.0)
+            }
+        }
 
-    fn serialize(&self, writer: &mut impl Write) -> Result<()> {
-        self.0.serialize(writer)
-    }
+        impl From<HashOutput> for $owned_type {
+            fn from(val: HashOutput) -> Self {
+                Self::from(Opaque::from(val))
+            }
+        }
+
+        impl<'a> From<$view_type<'a>> for HashOutputView<'a> {
+            fn from(val: $view_type<'a>) -> Self {
+                Self::from(val.0)
+            }
+        }
+
+        impl<'a> From<HashOutputView<'a>> for $view_type<'a> {
+            fn from(val: HashOutputView<'a>) -> Self {
+                Self::from(OpaqueView::from(val))
+            }
+        }
+    };
 }
 
-impl AsView for InitSecret {
-    type View<'a> = InitSecretView<'a>;
+to_from_hash_output! { EpochSecret + EpochSecretView }
+to_from_hash_output! { JoinerSecret + JoinerSecretView }
+to_from_hash_output! { MemberSecret + MemberSecretView }
+to_from_hash_output! { WelcomeSecret + WelcomeSecretView }
 
-    fn as_view<'a>(&'a self) -> Self::View<'a> {
-        InitSecretView(self.0.as_view())
-    }
-}
-
-#[derive(Clone, PartialEq, Debug)]
-pub struct InitSecretView<'a>(HashOutputView<'a>);
-
-impl<'a> Deserialize<'a> for InitSecretView<'a> {
-    fn deserialize(reader: &mut impl ReadRef<'a>) -> Result<Self> {
-        Ok(Self(HashOutputView::deserialize(reader)?))
-    }
-}
-
-impl<'a> ToOwned for InitSecretView<'a> {
-    type Owned = InitSecret;
-
-    fn to_owned(&self) -> Self::Owned {
-        InitSecret(self.0.to_owned())
-    }
-}
-
-pub struct EpochSecret(HashOutput);
-pub struct EpochSecretView<'a>(HashOutputView<'a>);
-
-pub struct KeyScheduleEpoch {
-    pub joiner_secret: HashOutput,
-    pub welcome_secret: HashOutput,
-    epoch_secret: EpochSecret,
-}
-
-impl<'a> InitSecretView<'a> {
+impl<'a> EpochSecretView<'a> {
     pub fn advance(
         self,
         commit_secret: HashOutputView,
         group_context: &GroupContext,
-    ) -> Result<(InitSecret, KeyScheduleEpoch)> {
+    ) -> Result<(EpochSecret, JoinerSecret, WelcomeSecret)> {
         let group_context = serialize!(GroupContext, group_context);
 
-        let pre_joiner_secret = crypto::extract(self.0, commit_secret);
-        let joiner_secret =
-            crypto::expand_with_label(pre_joiner_secret.as_view(), b"joiner", &group_context);
+        let joiner_secret = JoinerSecret::new(self, commit_secret, &group_context);
+        let member_secret = joiner_secret.as_view().advance();
+        let welcome_secret = member_secret.as_view().welcome_secret();
+        let epoch_secret = member_secret.as_view().advance(&group_context);
 
-        let psk_secret = HashOutput::zero();
-        let member_secret = crypto::extract(joiner_secret.as_view(), psk_secret.as_view());
-
-        let welcome_secret = crypto::derive_secret(member_secret.as_view(), b"welcome");
-
-        let epoch_secret =
-            crypto::expand_with_label(member_secret.as_view(), b"epoch", &group_context);
-
-        let init_secret = InitSecret(crypto::derive_secret(epoch_secret.as_view(), b"init"));
-
-        let epoch = KeyScheduleEpoch {
-            joiner_secret,
-            welcome_secret,
-            epoch_secret: EpochSecret(epoch_secret),
-        };
-
-        Ok((init_secret, epoch))
+        Ok((
+            epoch_secret.into(),
+            joiner_secret.into(),
+            welcome_secret.into(),
+        ))
     }
 }
 
-impl KeyScheduleEpoch {
+impl EpochSecret {
     pub fn confirmation_tag(&self, confirmed_transcript_hash: &HashOutput) -> HashOutput {
-        let confirmation_key = crypto::derive_secret(self.epoch_secret.0.as_view(), b"confirm");
+        let confirmation_key = crypto::derive_secret(self.as_view().into(), b"confirm");
         crypto::hmac(
             confirmation_key.as_ref(),
             confirmed_transcript_hash.as_ref(),
@@ -141,14 +134,14 @@ impl KeyScheduleEpoch {
     }
 
     pub fn sender_data_secret(&self) -> HashOutput {
-        crypto::derive_secret(self.epoch_secret.0.as_view(), b"sender data")
+        crypto::derive_secret(self.as_view().into(), b"sender data")
     }
 
     // XXX(RLB) This can be done immutably because we only ever derive one secret per epoch
     pub fn handshake_key(&self, index: LeafIndex, group_size: usize) -> (u32, AeadKey, AeadNonce) {
         let leaf = 2 * index.0 as usize;
         let mut parent = root(group_size);
-        let mut tree_secret = crypto::derive_secret(self.epoch_secret.0.as_view(), b"encryption");
+        let mut tree_secret = crypto::derive_secret(self.as_view().into(), b"encryption");
 
         loop {
             let Some(next) = step_towards(parent, leaf) else {
@@ -165,5 +158,40 @@ impl KeyScheduleEpoch {
         let generation = 0;
         let (key, nonce) = crypto::tree_key_nonce(handshake_secret.as_view(), generation);
         (generation, key, nonce)
+    }
+}
+
+impl JoinerSecret {
+    fn new(
+        epoch_secret: EpochSecretView,
+        commit_secret: HashOutputView,
+        group_context: &[u8],
+    ) -> Self {
+        let init_secret = crypto::derive_secret(epoch_secret.into(), b"init");
+        let pre_joiner_secret = crypto::extract(init_secret.as_view().into(), commit_secret);
+        crypto::expand_with_label(pre_joiner_secret.as_view(), b"joiner", &group_context).into()
+    }
+}
+
+impl<'a> JoinerSecretView<'a> {
+    fn advance(self) -> MemberSecret {
+        let psk_secret = HashOutput::zero();
+        crypto::extract(self.into(), psk_secret.as_view()).into()
+    }
+}
+
+impl<'a> WelcomeSecretView<'a> {
+    pub fn into_key_nonce(self) -> (AeadKey, AeadNonce) {
+        crypto::welcome_key_nonce(HashOutputView::from(self.0)).into()
+    }
+}
+
+impl<'a> MemberSecretView<'a> {
+    fn welcome_secret(&self) -> WelcomeSecret {
+        crypto::derive_secret(self.clone().into(), b"welcome").into()
+    }
+
+    fn advance(self, group_context: &[u8]) -> EpochSecret {
+        crypto::expand_with_label(self.into(), b"epoch", &group_context).into()
     }
 }
